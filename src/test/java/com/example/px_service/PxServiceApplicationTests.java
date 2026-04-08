c08d07d9b33508c32acab56777a44ce127e0f485
jarvenwang <wang_jin_bao@163.com>
2026-04-08 23:55:39 +0800

    初始化并修改配置参数

---
+++ src/test/java/com/example/px_service/PxServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.px_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PxServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
