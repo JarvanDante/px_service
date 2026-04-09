20ebee6d2c40fb413ad0505f369e57a2a820ca9b
jarvenwang <wang_jin_bao@163.com>
2026-04-09 15:38:10 +0800

    初始化第一个接口

---
+++ src/main/java/com/example/px_service/controller/DemoController.java
@@ -0,0 +1,14 @@
+package com.example.px_service.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @GetMapping("/hello")
+    public String Hello(){
+        return "px";
+    }
+
+}
