17bb862127d213b12338066b5f46d25a93f96d3d
jarvenwang <wang_jin_bao@163.com>
2026-04-12 18:56:50 +0800

    参数接收 + 参数校验

--- src/main/java/com/example/px_service/common/GlobalExceptionHandler.java
+++ src/main/java/com/example/px_service/common/GlobalExceptionHandler.java
@@ -1,10 +1,33 @@
 package com.example.px_service.common;
 
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ApiResponse handleValidationExceptions(MethodArgumentNotValidException ex) {
+        //获取第一个错误信息
+        String errorMsg = ex.getBindingResult()
+                .getAllErrors()
+                .get(0)
+                .getDefaultMessage();
+
+        return ApiResponse.error(errorMsg);
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ApiResponse handlerConstraintViolation(ConstraintViolationException ex) {
+        String errorMsg = ex.getConstraintViolations()
+                .iterator()
+                .next()
+                .getMessage();
+        return ApiResponse.error(errorMsg);
+    }
+
 
     @ExceptionHandler(Exception.class)
     public ApiResponse<?> handleException(Exception e) {
--- src/main/java/com/example/px_service/dto/LoginRequest.java
+++ src/main/java/com/example/px_service/dto/LoginRequest.java
@@ -1,9 +1,20 @@
 package com.example.px_service.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class LoginRequest {
 
+    @NotBlank(message = "用户名不能为空")
+    @Size(min = 5, max = 16, message = "用户名长度必须在5-16之间")
     private String username;
+
+    @NotBlank(message = "密码不能为空")
+    @Size(min = 6, max = 16, message = "用户名长度必须在5-16之间")
+//    @Min(value = 7, message = "密码必须大于等于7")
+//    @Max(value = 16, message = "密码必须小于等于16")
     private String password;
+
 
     public String getUsername() {
         return username;
