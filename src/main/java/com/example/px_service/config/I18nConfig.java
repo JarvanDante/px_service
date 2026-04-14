2629397f537e024f0e5f775226d5ddbdf2840091
jarvenwang <wang_jin_bao@163.com>
2026-04-14 16:04:03 +0800

    默认语言

--- src/main/java/com/example/px_service/config/I18nConfig.java
+++ src/main/java/com/example/px_service/config/I18nConfig.java
@@ -4,6 +4,10 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+
+import java.util.Locale;
 
 @Configuration
 public class I18nConfig {
@@ -15,5 +19,12 @@ public class I18nConfig {
         source.setDefaultEncoding("UTF-8");
         source.setFallbackToSystemLocale(false);
         return source;
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        AcceptHeaderLocaleResolver resolver = new AcceptHeaderLocaleResolver();
+        resolver.setDefaultLocale(Locale.US);
+        return resolver;
     }
 }
