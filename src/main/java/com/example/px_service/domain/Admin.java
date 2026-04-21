f84c8a7351389d6637f1d256675dc6d4dad7a0b1
jarvenwang <wang_jin_bao@163.com>
2026-04-22 01:37:45 +0800

    Lombok优化

--- src/main/java/com/example/px_service/domain/Admin.java
+++ src/main/java/com/example/px_service/domain/Admin.java
@@ -1,7 +1,12 @@
 package com.example.px_service.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.time.Instant;
 
+@Getter
+@Setter
 public class Admin {
     //主键自增
     private Integer id;
@@ -18,119 +23,6 @@ public class Admin {
     private String lastLoginIp;
     private Instant lastLoginTime;
     private Integer deleteAt;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public Integer getSiteId() {
-        return siteId;
-    }
-
-    public void setSiteId(Integer siteId) {
-        this.siteId = siteId;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getNickname() {
-        return nickname;
-    }
-
-    public void setNickname(String nickname) {
-        this.nickname = nickname;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Integer getAdminRoleId() {
-        return adminRoleId;
-    }
-
-    public void setAdminRoleId(Integer adminRoleId) {
-        this.adminRoleId = adminRoleId;
-    }
-
-    public Integer getStatus() {
-        return status;
-    }
-
-    public void setStatus(Integer status) {
-        this.status = status;
-    }
-
-    public Integer getSwitchGoogle2f() {
-        return switchGoogle2f;
-    }
-
-    public void setSwitchGoogle2f(Integer switchGoogle2f) {
-        this.switchGoogle2f = switchGoogle2f;
-    }
-
-    public String getGoogle2faSecret() {
-        return google2faSecret;
-    }
-
-    public void setGoogle2faSecret(String google2faSecret) {
-        this.google2faSecret = google2faSecret;
-    }
-
-    public String getLastLoginIp() {
-        return lastLoginIp;
-    }
-
-    public void setLastLoginIp(String lastLoginIp) {
-        this.lastLoginIp = lastLoginIp;
-    }
-
-    public Instant getLastLoginTime() {
-        return lastLoginTime;
-    }
-
-    public void setLastLoginTime(Instant lastLoginTime) {
-        this.lastLoginTime = lastLoginTime;
-    }
-
-    public Integer getDeleteAt() {
-        return deleteAt;
-    }
-
-    public void setDeleteAt(Integer deleteAt) {
-        this.deleteAt = deleteAt;
-    }
-
-    public Instant getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(Instant createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public Instant getUpdatedAt() {
-        return updatedAt;
-    }
-
-    public void setUpdatedAt(Instant updatedAt) {
-        this.updatedAt = updatedAt;
-    }
-
     private Instant createdAt;
     private Instant updatedAt;
 
